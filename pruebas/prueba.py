343d3b6c718313d28968db84f585063f7615f333
Gabriel Oriol Fernández Rodriguez <Gabriel Fernndez>
2016-03-29 10:55:55 -0300

    Agregados TODOs.

--- pruebas/prueba.py
+++ pruebas/prueba.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#TODO  Este es para Nacho: crear una variable que guarde el valor de una suma
+#  cualquiera y luego que se muestre en la pantalla
+
+#TODO  Esto es para Romina: crear una lista con 5 elementos cualquiera. 
+
 if __name__ == '__main__':
     print "Hola Mundo!"
