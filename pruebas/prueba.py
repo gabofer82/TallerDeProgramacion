2df9c27a2a1c27a52122f15fe16f4290ba4e0134
Gabriel Oriol Fernández Rodriguez <Gabriel Fernndez>
2016-04-04 21:38:38 -0300

    Cambios para Guillermo.

--- pruebas/prueba.py
+++ pruebas/prueba.py
@@ -4,7 +4,12 @@
 #TODO  Este es para Nacho: crear una variable que guarde el valor de una suma
 #  cualquiera y luego que se muestre en la pantalla
 
-#TODO  Esto es para Romina: crear una lista con 5 elementos cualquiera. 
+#TODO  Esto es para Romina: crear una lista con 5 elementos cualquiera.
+
+#TODO Esto es para Guillermo: crear un diccionario con 5 elementos cualquiera
+
+# PARA TODOS: lo que escriban debe estar en el bloque del condicional if de
+# abajo...
 
 if __name__ == '__main__':
     print "Hola Mundo!"
